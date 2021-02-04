df3212f3ab5393554793bb33a6f27036edeee8fb
tmaret <tmaret@adobe.com>
2021-02-04 12:48:44 +0100

    Add workflow app

--- README.md
+++ README.md
@@ -1,43 +1,36 @@
 # OAuth 2.0 server to server authentication to ​AEM as a Cloud Service​
 
-This demo leverages the [AEM-CS API Client Library](https://github.com/adobe/aemcs-api-client-lib) to
-authenticate a server to an [AEM as a Cloud Service](https://experienceleague.adobe.com/docs/experience-manager-cloud-service/overview/introduction.html) environment.
-
-TBD - pitch
+This demo leverages server to server authentication to allow a Node.js app to authenticate and update content maintained in an [AEM as a Cloud Service](https://experienceleague.adobe.com/docs/experience-manager-cloud-service/overview/introduction.html) environment. The app simulates a workflow engine that periodically schedules the `On Time` activation date of an asset. The app leverages the [AEM-CS API Client Library](https://github.com/adobe/aemcs-api-client-lib).
 
 ## Requirements
 
-* Access to an AEM as a Cloud Service environment
+* Access to an environment in AEM as a Cloud Service with the [WKND](https://experienceleague.adobe.com/docs/experience-manager-learn/getting-started-wknd-tutorial-develop/overview.html) demo content
 
-# Setup
+* Service Credentials or Local Development Token as described in [Generating Access Tokens for Server Side APIs](https://experienceleague.adobe.com/docs/experience-manager-cloud-service/implementing/developing/generating-access-tokens-for-server-side-apis.html?lang=en#the-server-to-server-flow)
 
-## Install the server
+* Technical Account added in the `content-authors` group via the AEM Security Admin UI.
+
+## Install the workflow app
 
 ```
 npm install
 ```
 
-## Start the server
+## Start the workflow app
 
 ```
-node index.js
+node workflow.js --aem https://author-p19554-e28364-cmstg.adobeaemcloud.com \
+                 --path /content/dam/wknd/en/adventures/bali-surf-camp/AdobeStock_175749320.jpg \
+                 --file service_token_cm-p19554-e28364.json
 ```
 
 ## Help
 
 ```
-node index.js -h
-Options:
-      --version                Show version number                     [boolean]
-      --aem, --aem_author_url  AEM Author URL
-      [string] [default: "https://author-p19554-e28364-cmstg.adobeaemcloud.com"]
-      --file, --cred_file      File containing Service Credentials obtained via
-                               the Developer Console
-                       [string] [default: "service_token_cm-p19554-e28363.json"]
-  -h, --help                   Show help                               [boolean]
+node workflow.js -h
 ```
 
-# Links
+## Links
 
 * [AEM-CS API Client Library](https://github.com/adobe/aemcs-api-client-lib)
 * [Generating Access Tokens for Server Side APIs](https://experienceleague.adobe.com/docs/experience-manager-cloud-service/implementing/developing/generating-access-tokens-for-server-side-apis.html?lang=en#the-server-to-server-flow)
---
+++ workflow.js
@@ -0,0 +1,101 @@
+/*jshint node:true */
+"use strict";
+
+/*
+Copyright 2020 Adobe. All rights reserved.
+This file is licensed to you under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License. You may obtain a copy
+of the License at http://www.apache.org/licenses/LICENSE-2.0
+Unless required by applicable law or agreed to in writing, software distributed under
+the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR REPRESENTATIONS
+OF ANY KIND, either express or implied. See the License for the specific language
+governing permissions and limitations under the License.
+*/
+
+const fs = require('fs')
+const yargs = require('yargs')
+const exchange = require('@adobe/aemcs-api-client-lib')
+
+const FormData = require('form-data')
+const fetch = require('node-fetch')
+
+const NodeCache = require( "node-cache" )
+const cache = new NodeCache()
+
+const argv = yargs(process.argv.slice(2))
+    .option('aem', {
+        description: 'AEM Author URL',
+        type: 'string',
+        default: 'https://author-p19554-e28364-cmstg.adobeaemcloud.com'
+    })
+    .option('path', {
+        description: 'Path of the asset to be updated',
+        type: 'string',
+        default: '/content/dam/wknd/en/adventures/bali-surf-camp/AdobeStock_175749320.jpg'
+    })
+    .option('file', {
+        description: 'File containing Service Credentials obtained via the Developer Console',
+        type: 'string',
+        default: 'service_token_cm-p19554-e28364.json'
+    })
+    .help().alias('help', 'h')
+    .argv;
+
+var config = JSON.parse(fs.readFileSync(argv.file, 'utf8'))
+
+/*
+ * Run the workflow every 30 seconds
+ */
+setInterval(updateOnTime, 30 * 1000);
+
+/*
+ * Update `onTime` property using the Apache Sling Post Servlet
+ *
+ * https://sling.apache.org/documentation/bundles/manipulating-content-the-slingpostservlet-servlets-post.html
+ */
+async function updateOnTime() {
+    getAccessToken().then(token => {
+        const form = new FormData()
+        const onTime = new Date().toISOString()
+        form.append('onTime', onTime)
+        fetch(`${argv.aem}${argv.path}/jcr:content`, {
+            method: 'POST',
+            headers: {
+                'Authorization': `Bearer ${token}`
+            },
+            body: form
+        }).then(res => {
+            if (res.status === 200) {
+                console.log(`Updated onTime with '${onTime}' for ${argv.path}`)
+            } else {
+                console.error(`Failed to update onTime ${res.status} ${res.statusText}`)
+            }
+        });
+    })
+}
+
+/*
+ * Get an access token from the `Local Development Token` config
+ * or by performing an exchange using the `Service Credentials` config
+ */
+async function getAccessToken() {
+    return config.accessToken || exchangeAccessToken()
+}
+
+/*
+ * Get an access token via the JWT exchange flow.
+ *
+ * The access token is reused until it has expired.
+ */
+async function exchangeAccessToken() {
+    const token = cache.get('token')
+    return (undefined !== token) ? token : exchange(config).then(res => {
+        // cache the access token with a ttl that account for a 5% leeway
+        const ttl = Math.round(res.expires_in / 1000 * 0.95)
+        cache.set('token', res.access_token, ttl)
+        return res.access_token
+    }).catch(e => {
+        console.log("Failed to exchange for access token ", e)
+        throw e;
+    });
+}
